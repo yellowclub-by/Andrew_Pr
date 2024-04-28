9b736affd24a580eb4fd756956c1dce97c52aa4b
YellowClub <hellow@yellowclub.by>
2024-04-28 13:05:24 +0300

    Lesson 1 - Создание первого echo bot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('My first Andrews project on Python')
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+TOKEN = "7127422770:AAGqCQ1JjxdF4Td36Oj1WyDodUoE7UfmrIE"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer('Привет, это бот clothes_shop_MInsk')
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer('бот пока находится в разработке')
+    user_text = message.text
+    await message.answer(user_text)
+
+
+async def main():
+    await  dp.start_polling(bot)
+
+
+asyncio.run(main())
