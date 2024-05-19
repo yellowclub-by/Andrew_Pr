838899ae6dd9e66b700e6e7d2fc4aae2c620d4e8
YellowClub <hellow@yellowclub.by>
2024-05-19 13:06:27 +0300

    Lesson 4 - фильтрация сообщений по словам/удаление

---
+++ handlers/user_group.py
@@ -0,0 +1,15 @@
+from aiogram import types, Router, F
+
+group_router = Router()
+
+restricted_words = ['козьел', 'персик', 'шоколад', 'фурри', 'рентгеноэлектрокардиографического', 'превысокомногорассмотрительствующий']
+
+
+@group_router.message(F.text)
+async def cleaner(message: types.Message):
+    words_lst = message.text.split(" ")
+    for word in words_lst:
+        if word.lower() in restricted_words:
+            await message.answer(f'{message.from_user.first_name} соблюдайте правила чата!')
+            await message.delete()
+            break
